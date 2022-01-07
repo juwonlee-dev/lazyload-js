3bd1a5bd3d3efbcd21a3a161fc788e61974f3e83
Juwon Lee <47394520+juwonlee-dev@users.noreply.github.com>
2022-01-07 16:11:34 +0900

    Update lazyload-lib-min.js
    변경사항 반영

--- lazyload-lib-min.js
+++ lazyload-lib-min.js
@@ -1,11 +1,11 @@
 /*
- * jQuery JavaScript LazyLoad Library v1.0.0
+ * jQuery JavaScript LazyLoad Library v1.0.1
  * https://github.com/juwonlee-dev/lazyload-js
  *
  * Copyright (c) 2021 JuwonLee
  * Single licensed under the GPL 2.0 licenses.
  * https://github.com/juwonlee-dev/lazyload-js/blob/main/LICENSE
  *
- * Date: Tue Aug 24 2021 20:30:00 GMT+0900 (한국 표준시)
+ * Date: Fri Jan 07 2022 16:09:20 GMT+0900 (한국 표준시)
  */
-(function(){inject_code()})(jQuery);function inject_code(){"use strict";$.getScripts||($.getScripts=function(a){var b,c,d,e,f;if(b=$.extend({async:!1,cache:!0},a),"string"==typeof b.urls&&(b.urls=[b.urls]),f={done:[],fail:[]},c=function(){$.ajax({url:b.urls.shift(),dataType:"script",cache:b.cache,success:function(){var d=arguments;d[3]=this.url,f.done.push(d),0<b.urls.length?c():"function"==typeof a.success&&a.success(f)},error:function(){var d=arguments;d[3]=this.url,f.fail.push(d),0<b.urls.length?c():"function"==typeof a.error&&a.error(f)}})},d=function(){var c=arguments;c[3]=this.url,f.done.push(c),f.done.length===b.urls.length&&"function"==typeof a.success&&a.success(f)},e=function(){var c=arguments;c[3]=this.url,f.fail.push(c),f.fail.length===b.urls.length&&"function"==typeof a.error&&a.error(f)},!0===b.async)for(var g=0;g<b.urls.length;g++)$.ajax({url:b.urls[g],dataType:"script",cache:b.cache,success:d,error:e});else c()})}
+function inject_code(){"use strict";$.getScripts||($.getScripts=function(e){var r,s,c,t,n;if((r=$.extend({async:!1,cache:!0},e)).urls.length<1)console.error("[lazyload] urls parameter undefined");else if("string"==typeof r.urls&&(r.urls=[r.urls]),n={done:[],fail:[]},s=function(){$.ajax({url:r.urls.shift(),dataType:"script",cache:r.cache,success:function(){var c=arguments;c[3]=this.url,n.done.push(c),r.urls.length>0?s():"function"==typeof e.success&&e.success(n)},error:function(){var c=arguments;c[3]=this.url,n.fail.push(c),r.urls.length>0?s():"function"==typeof e.error&&e.error(n)}})},c=function(){var s=arguments;s[3]=this.url,n.done.push(s),n.done.length===r.urls.length&&"function"==typeof e.success&&e.success(n)},t=function(){var s=arguments;s[3]=this.url,n.fail.push(s),n.fail.length===r.urls.length&&"function"==typeof e.error&&e.error(n)},!0===r.async)for(var u=0;u<r.urls.length;u++)$.ajax({url:r.urls[u],dataType:"script",cache:r.cache,success:c,error:t});else s()})}jQuery,inject_code();
